41f75727789f742af0c747691370935ef7080604
volodix <58565511+volodix@users.noreply.github.com>
2019-12-07 21:22:11 +0300

    Add files via upload

---
+++ Reklama/script.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName('consult-but')[0].onclick = () => {
+    document.getElementsByClassName('overlay')[0].style.display = 'flex';
+  }
+  document.getElementsByClassName('popup-close')[0].onclick = () => {
+    document.getElementsByClassName('overlay')[0].style.display = 'none';
+  }
