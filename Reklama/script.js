ee758eb2909a803c2745f4a3e7543f88da8d27c8
volodix <58565511+volodix@users.noreply.github.com>
2019-12-09 11:19:29 +0300

    Add files via upload

--- Reklama/script.js
+++ Reklama/script.js
@@ -4,3 +4,18 @@ document.getElementsByClassName('consult-but')[0].onclick = () => {
   document.getElementsByClassName('popup-close')[0].onclick = () => {
     document.getElementsByClassName('overlay')[0].style.display = 'none';
   }
+
+  const anchors = document.querySelectorAll('a[href*="#"]')
+
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+      
+      const blockID = anchor.getAttribute('href').substr(1)
+      
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  }
