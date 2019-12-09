7aa5c3c38af643f32159a1ec95e2cdef9b16c79e
volodix <58565511+volodix@users.noreply.github.com>
2019-12-09 11:19:02 +0300

    Add files via upload

--- Hilights/script.js
+++ Hilights/script.js
@@ -5,3 +5,18 @@ document.getElementsByClassName('fa-bars')[0].onclick = () => {
   document.getElementsByClassName('close-side-nav')[0].onclick = () => {
     document.getElementsByClassName('side-nav')[0].style.right = '-300px';
   }
+
+  const anchors = document.querySelectorAll('a[href*="#"]')
+
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+      
+      const blockID = anchor.getAttribute('href').substr(1)
+      
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  }
